13430c82374cbb45e7403996ec65795be5420a33
Nicholas Mullins <nicholasmullins@gmail.com>
2021-04-01 00:34:14 -0600

    20210331235227 - Added Verification for Two Labeled Element, Added Verifications for all Basic Gestures in the Individual Scripts, Got all Verifications to work except for Two Strings which I slacked the channel about

--- lkg/scripts/verify_two_labels.py
+++ lkg/scripts/verify_two_labels.py
@@ -2,5 +2,5 @@ import logging
 
 
 def run(context):
-    context.verify(labels=["btn_accept", "btn_ok"])
-    context.perform_gesture('tap', 'btn_verified')
+    context.verify(labels=["btn_accept", "btn_modal_button"], label_count=1)
+    context.perform_gesture('tap', 'btn_accept')
